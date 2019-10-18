49dcf58691ae15489c2562afc5e2f6be585c7849
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2019-10-17 23:54:35 -0400

    More boilerplate configuration and package setup

---
+++ setup.py
@@ -0,0 +1,46 @@
+import os
+from setuptools import setup, find_packages
+
+PACKAGE_NAME = "pylifttk"
+
+# The text of the README file
+README = open("README.md").read()
+
+# Get the version number without importing our package
+# (which would trigger some ImportError due to missing dependencies)
+
+version_contents = {}
+with open(os.path.join(PACKAGE_NAME, "version.py")) as f:
+    exec(f.read(), version_contents)
+
+# This call to setup() does all the work
+setup(
+    name=PACKAGE_NAME,
+    version=version_contents["__version__"],
+    description="Python utility toolkit for Princeton CS's LIFT.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jlumbroso/pylifttk",
+    author="Jérémie Lumbroso",
+    author_email="lumbroso@cs.princeton.edu",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=find_packages(),
+    install_requires=[
+        "confuse",
+        "python-dateutil",
+        "pywsse",
+        "PyYAML",
+        "requests",
+        "six",
+        # "better_exceptions",
+        # "blessings",
+        # "colorama",
+        # "eliot",
+    ],
+    include_package_data=True,
+)
